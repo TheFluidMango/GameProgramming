c26c6e45649ba946ded295cde13580e464ec2d20
Adam Smith <TheFluidMango@Adams-MacBook-Pro.local>
2016-02-01 12:08:40 -0800

    initial

---
+++ Learning1/Assets/Scripts/Interact.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interact : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.name.StartsWith("Ball")) {
+			Color color = new Color (Random.value, Random.value, Random.value);
+			MeshRenderer gameObjectRenderer = this.GetComponent<MeshRenderer>();
+			gameObjectRenderer.material.color = color;
+
+			Destroy (other.gameObject);
+
+			//Remove this comment to destroy the object when it's hit
+			//Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Learning1/Assets/Scripts/Throw.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throw : MonoBehaviour {
+
+	public Rigidbody objectToThrow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	// Update is called once per frame
+	void Update () {
+
+		if (objectToThrow != null) {
+			if (Input.GetButtonUp ("Fire1")) {
+				Debug.Log ("FIRE!");
+				Rigidbody ball = Instantiate (objectToThrow, transform.position, transform.rotation) as Rigidbody;
+				ball.velocity = transform.TransformDirection (Vector3.forward * 10);
+			}
+		}
+	}
+}
