d7c87d8c5060310cd184bba5a7d4f707aa56679b
Adam Smith <TheFluidMango@Adams-MacBook-Pro.local>
2016-02-03 11:59:09 -0800

    second demo

---
+++ Learning1/Assets/Scripts/Fire.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+	public GameObject firePoint;
+	public Collector collector;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.name.StartsWith ("Ball")) {
+			if (collector != null && firePoint != null) {
+				GameObject objectToFire = collector.getNext ();
+				if (objectToFire != null) {
+					Rigidbody ball = objectToFire.GetComponent<Rigidbody>();
+					ball.transform.position = firePoint.transform.position;
+					Transform canon = firePoint.transform.parent;
+					ball.velocity = canon.TransformDirection (-Vector3.forward * 40);
+					Destroy (other.gameObject);
+				}
+			} else {
+				Debug.Log ("Error: Set firePoint and collector");
+			}
+		}
+	}
+
+
+
+}
--- Learning1/Assets/Scripts/Throw.cs
+++ Learning1/Assets/Scripts/Throw.cs
@@ -1,23 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Throw : MonoBehaviour {
 
 	public Rigidbody objectToThrow;
+	public List<Color> colorList;
 
 	// Use this for initialization
 	void Start () {
-	
+		List<int> newList = new List<int> ();
+		newList.Add (5);
+		newList.Add (6);
+		newList.Add (7);
+		newList.Remove (6);
+		for (int i = 0; i < newList.Count; i++) {
+			Debug.Log (newList [i]);
+		}
+
 	}
-	
-	// Update is called once per frame
+
 	// Update is called once per frame
 	void Update () {
 
 		if (objectToThrow != null) {
 			if (Input.GetButtonUp ("Fire1")) {
-				Debug.Log ("FIRE!");
 				Rigidbody ball = Instantiate (objectToThrow, transform.position, transform.rotation) as Rigidbody;
+
+				MeshRenderer renderer = ball.GetComponent<MeshRenderer> ();
+				Color newColor = new Color (Random.value, Random.value, Random.value);
+				if (colorList.Count > 0) {
+					newColor = colorList [(int)(Random.value * colorList.Count)];
+				}
+
+				renderer.material.color = newColor;
 				ball.velocity = transform.TransformDirection (Vector3.forward * 10);
 			}
 		}
